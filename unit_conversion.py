975f5efd7611e0adb522cc1d88b9c4a2ddcaaac2
Shahid.M <shahid.mulla1@outlook.com>
2025-08-08 21:42:58 +0100

    new function started

--- unit_conversion.py
+++ unit_conversion.py
@@ -3,15 +3,15 @@ print("Select what category of unit that are you trying to convert from - ")
 
 def menu():
     print("Options are listed below;")
-    ("1. Distance / Length")
-    ("2. Weight / Mass")
-    ("3. Temperature")
-    ("4. Area")
-    ("5. Volume")
-    ("6. Time")
-    ("7. Speed")
-    ("8. Pressure")
-    ("9. Energy")
+    print("1. Distance / Length")
+    print("2. Weight / Mass")
+    print("3. Temperature")
+    print("4. Area")
+    print("5. Volume")
+    print("6. Time")
+    print("7. Speed")
+    print("8. Pressure")
+    print("9. Energy")
 
     while True:
         category_1 = int(input("Enter your option number : "))
@@ -42,3 +42,5 @@ def menu():
     return (category_1)
 
 menu()
+
+def distance_or_length(category_1):
