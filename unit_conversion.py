8f66046824e4b7dfd383110c951acdf15ff38cdc
Shahid.M <shahid.mulla1@outlook.com>
2025-08-11 10:04:05 +0100

    fixing problems with nested functions

--- unit_conversion.py
+++ unit_conversion.py
@@ -15,9 +15,9 @@ def menu():
 
     while True:
         try:
-            category_1 = int(input("Enter your option number : "))
+            category = int(input("Enter your option number : "))
             thisdict ={
-                1 : "Distance / Length",
+                1: "Distance / Length",
                 2: "Weight / Mass",
                 3: "Temperature",
                 4: "Area",
@@ -28,51 +28,49 @@ def menu():
                 9: "Energy"
 
             }
-            if 0 <category_1 <= 9:
-                print("-{You have selected the valid option number", category_1, "corresponding to", thisdict[category_1], ".}-\n")
-                selected_category = thisdict[category_1]
+            if 0 <category <= 9:
+                print("-{You have selected the valid option number", category, "corresponding to", thisdict[category], ".}-\n")
+                selected_category = thisdict[category]
                 break
             else:
                 print("[[Please select a number within the range 1 - 9.]]")
                 print("[[Try again]]\n")
+                continue
         except ValueError:
              print("[[You have chosen an invalid option or reponded incorrectly]]")
              print("[[Please try again.]]\n")
              continue
-    return (selected_category)
+    return(selected_category)
 
-menu()
 
-def distance_or_length_conversion_module(selected_category):
-    while True:
-        if selected_category != "Distance / Length":
-            break
-        else:
-            continue
-    def unit_selection_1():
-        while True:
-            print("From the following measurement category select the unit you would like to convert from;")
-            print("1. Meters (m) ")
-            print("2. Kilometers (km)")
-            print("3. Miles (M)")
-            print("4. Feet (ft)")
-            print("5. Inches (in)")
-            print("6. Centimeters (cm)")
-            print("7. Milimeters (mm)\n")
-            try: 
-                unit_choice_1 = int(input("Enter option number here:"))
-                if unit_choice_1 > 0 and unit_choice_1 <= 7:
-                    print("-{You have successfully selected a valid option!}-")
-                    break
-                elif unit_choice_1 < 0 or unit_choice_1 > 9:
-                    print("[[Please try again by entering an existing value]]")
-                    continue
-            except ValueError:
-                print("[[Please try again and enter a valid numerical value]]")
-                continue
-            return (unit_choice_1)
-        return(selected_category)
-    test = distance_or_length_conversion_module(selected_category)
-    test()
 
-                
+def distance_or_length_conversion_module(user_choice):
+    if user_choice != "Distance / Length":
+        print("[[category 1 has not been selected, conversion not performed.]]")
+        return None
+    else:
+        def unit_selection_1():
+                while True:
+                    print("From the following measurement category select the unit you would like to convert from;")
+                    print("1. Meters (m) ")
+                    print("2. Kilometers (km)")
+                    print("3. Miles (M)")
+                    print("4. Feet (ft)")
+                    print("5. Inches (in)")
+                    print("6. Centimeters (cm)")
+                    print("7. Milimeters (mm)\n")
+                    try: 
+                        unit_choice_1 = int(input("Enter option number here:"))
+                        if unit_choice_1 > 0 and unit_choice_1 <= 7:
+                            print("-{You have successfully selected a valid option!}-")
+                            break
+                        elif unit_choice_1 < 0 or unit_choice_1 > 9:
+                            print("[[Please try again by entering an existing value]]")
+                            continue
+                    except ValueError:
+                        print("[[Please try again and enter a valid numerical value]]")
+                        continue
+                return unit_selection_1()
+
+user_choice_1 = menu()
+distance_or_length_conversion_module(user_choice_1)
